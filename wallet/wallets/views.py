da2c95a85ad0fc5f8bdf51a82d4626a51bba8676
Torham <torhamdev@gmail.com>
2024-06-16 11:05:58 -0700

    🔨 Add deposit API

--- wallet/wallets/views.py
+++ wallet/wallets/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from wallets.models import Wallet
-from wallets.serializers import WalletSerializer
+from wallets.serializers import WalletDepositSerializer, WalletSerializer
 
 
 class CreateWalletView(CreateAPIView):
@@ -17,9 +18,15 @@ class RetrieveWalletView(RetrieveAPIView):
 
 
 class CreateDepositView(APIView):
-    def post(self, request, *args, **kwargs):
-        # todo: update the wallet's balance and return proper response
-        return Response({})
+    def post(self, request, uuid, *args, **kwargs):
+        wallet = get_object_or_404(Wallet, uuid=uuid)
+
+        serializer = WalletDepositSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        wallet.deposit(amount=serializer.data["amount"])
+
+        return Response(serializer.data)
 
 
 class ScheduleWithdrawView(APIView):
